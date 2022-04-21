1b03c036ccbda9aceba067775a5c440294b7ae4b
Bashar-Owainat <owainatbashar13@gmail.com>
2022-04-21 23:15:06 +0300

    lab 09

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using static LINQ.MyModel;
 
 namespace LINQ
 {
+   
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            string fileName = "data.json";
+            string jsonString = File.ReadAllText(fileName);
+
+          var data = Newtonsoft.Json.JsonConvert.DeserializeObject<MyModel>(jsonString);
+
+
+         
+            foreach (var item in data)
+            {
+               Console.WriteLine(item);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
