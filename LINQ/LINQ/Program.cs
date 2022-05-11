760bcfe817c91a02e79517320050d78cdfc1bdfe
Bashar-Owainat <owainatbashar13@gmail.com>
2022-05-11 14:33:21 +0300

    done

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -21,14 +21,71 @@ namespace LINQ
             string fileName = "data.json";
             string jsonString = File.ReadAllText(fileName);
 
-          var data = Newtonsoft.Json.JsonConvert.DeserializeObject<MyModel>(jsonString);
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject<MyModel>(jsonString);
+
+            var features = from allFeatures in data.features select allFeatures;
+
+            
+            var allneighborhoods = from neighborhood in features select neighborhood.properties.neighborhood; //LINQ query
+
+            var neighborsWithNames = allneighborhoods.Where(neighbors => neighbors != ""); //LINQ method
+
+            var properties = from property in features select property.properties;
+
+            var notDuplicated = properties.GroupBy(x => x.neighborhood).Select(y => y.First());
+
+            var notDuplicatedAndNotEmpty = properties.Where(property => property.neighborhood != "").GroupBy(x => x.neighborhood).Select(y => y.First());
 
 
-         
-            foreach (var item in data)
+           
+            var rewiteNeighborsWithNames = from property in properties where property.neighborhood != "" select property.neighborhood;
+
+
+
+
+
+            Console.WriteLine("Output all of the neighborhoods in this data list");
+            foreach (var item in allneighborhoods)
             {
-               Console.WriteLine(item);
+                Console.WriteLine(item);
             }
+
+
+
+            //Console.WriteLine("Filter out all the neighborhoods that do not have any names ");
+
+            //foreach (var item in neighborsWithNames)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+
+            //Console.WriteLine("Remove the duplicates ");
+
+            //foreach (var item in notDuplicated)
+            //{
+            //    Console.WriteLine(item.neighborhood);
+            //}
+
+
+
+            //Console.WriteLine("Rewrite the queries from above and consolidate all into one single query ");
+
+            //foreach (var item in notDuplicatedAndNotEmpty)
+            //{
+            //    Console.WriteLine(item.neighborhood);
+            //}
+
+
+
+            //Console.WriteLine("Rewrite at least one of these questions only using the opposing method ");
+
+            //foreach (var item in rewiteNeighborsWithNames)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
 
 
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # lab09-LINQ
 ## Summary 
 
-In this app I read data from an external json file, I seperated them in classes and then I deserialized them.
+In this app I read data from an external json file, I seperated them in classes and then I deserialized them and finally I did operations over them.
+
+## Results
+
+Output all of the neighborhoods in this data list 
+
+![](allNeighborhoods.png)
+
+Filter out all the neighborhoods that do not have any names
+
+![](withNames.png)
+
+Remove the duplicates 
+
+![](withoutDuplicates.png)
+
+Rewrite the queries from above and consolidate all into one single query.
+![](noDuplicatesWithNames.png)
+
+Rewrite at least one of these questions only using the opposing method
+![](withNames.png)
